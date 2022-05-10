663e962dd3b23257109e800da0ff452595fe8cfe
itsDamon <nick.parise20@gmail.com>
2022-05-10 09:00:07 +0200

    JS adding

---
+++ JS/myFunction.js
@@ -0,0 +1,21 @@
+const actualTime = moment();
+
+function dayDifferenceF(d) {
+    let dayDifference = actualTime.diff(d, "days");
+    return dayDifference;
+}
+
+function hoursDifferenceF(d){
+    let hoursDifference = actualTime.diff(d, "hours") - actualTime.diff(d, "days")*24;
+    return hoursDifference;
+}
+
+function minutesDifferenceF(d) {
+    let minutesDifference = actualTime.diff(d, "minutes") - (actualTime.diff(d, "hours")*60);
+    return minutesDifference;
+}
+
+function secondsDifferenceF(d) {
+    let secondDifference = actualTime.diff(d, "seconds") - (actualTime.diff(d, "minutes")*60);
+    return secondDifference;
+}
